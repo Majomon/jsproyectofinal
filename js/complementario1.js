929446315e9118dad0414d5d63ab54bb878267eb
Mauri <mauri.monzon91@gmail.com>
2022-06-16 11:18:45 -0300

    testeado

--- js/complementario1.js
+++ js/complementario1.js
@@ -15,23 +15,49 @@ class Producto {
         this.id = id;
         this.nombre = nombre.toUpperCase();
         this.precio = parseFloat(precio);
-        this.stock = parseInt(stock);
     }
 }
 
 
 const productos = [];
+salida=""
 
-console.log("Programa para ingresar productos");
 
-let entrada = prompt("Cuantos productos va agregar?")
+
+let entrada = prompt("Programa para ingresar productos \nCuantos productos va agregar?")
 
 for (let index = 0; index < entrada; index++) {
     let idProducto = index + 1
     let nombreProducto = prompt("Ingrese nombre del producto")
     let precioProducto = prompt("Ingrese precio del producto")
-    let stockProducto = prompt("Ingrese el stock")
-    productos.push(new Producto(idProducto, nombreProducto, precioProducto, stockProducto));
+    productos.push(new Producto(idProducto, nombreProducto, precioProducto));
 }
-console.log("Detalle de los productos ingresados");
+console.log("Los productos ya fueron cargados");
 console.log(productos);
+console.log("");
+
+
+do{
+    let elementoProducto = prompt("Ingrese el nombre del producto que desea comprar?")
+    elementoProducto=elementoProducto.toUpperCase()
+    const filtroProducto= productos.filter((el)=>el.nombre.includes(elementoProducto))
+    
+    let cantidadProducto = parseInt(prompt("Que cantidad quiere?"))
+    
+    const precioPorCantidad= filtroProducto.map((el)=>{
+        return{
+            id: el.id,
+            nombre: el.nombre,
+            precio: el.precio * cantidadProducto
+        }
+    })
+    
+    console.log(precioPorCantidad);
+    
+    salida=prompt("Si quiere realizar otra compra presiones cualquier tecla \n Si quiere salir escriba ESC")
+
+}while (salida !="ESC")
+
+console.log("Fin de la compra");
+
+
